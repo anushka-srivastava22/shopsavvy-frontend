afa6b67308c1f30d912240f88a46697ab011b448
anushka-srivastava22 <srivastavaanushka3@gmail.com>
2023-12-28 22:22:50 +0530

    first commit

--- src/pages/user/Orders.js
+++ src/pages/user/Orders.js
@@ -10,7 +10,7 @@ const Orders = () => {
   const [auth, setAuth] = useAuth();
   const getOrders = async () => {
     try {
-      const { data } = await axios.get("http://localhost:8080/api/v1/auth/orders");
+      const { data } = await axios.get("https://shopsavvy-backend.onrender.com/api/v1/auth/orders");
       //const { data } = await axios.get(" http://localhost:8080/api/v1/auth/orders");
       setOrders(data);
     } catch (error) {
